f91ee1c72d56580ce3c7753652d329cdd3cce115
Арсений Водяхин <164699@fa.ru>
2020-06-22 14:26:26 +0300

    Initial commit

--- DriveMyself/DriveMyself/Properties/Resources.Designer.cs
+++ DriveMyself/DriveMyself/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Demo.Properties
+namespace DriveMyself.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace Demo.Properties
 			{
 				if ((resourceMan == null))
 				{
-					global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Demo.Properties.Resources", typeof(Resources).Assembly);
+					global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DriveMyself.Properties.Resources", typeof(Resources).Assembly);
 					resourceMan = temp;
 				}
 				return resourceMan;
